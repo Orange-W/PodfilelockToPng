528a88c22977c5f11495abd97d96efabdbd02b11
OrangeEvan <495871401@163.com>
2023-06-09 16:13:26 +0800

    Update printlock.sh
    更新 脚本添加参数

--- printlock.sh
+++ printlock.sh
@@ -3,7 +3,40 @@ IFS=''
 file=${1:-"Podfile.lock"}
 output=${2:-"output.dot"}
 
-disableSubLine=${3:-0}
+disableSubLine=0
+onlyName=''
+
+while [[ $# -gt 0 ]]; do
+    key="$1"
+
+    case $key in
+        -i|--input)
+        file="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        -o|--output)
+        output="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        -n|--no_sub)
+        disableSubLine=1
+        shift # past argument
+        ;;
+        --only)
+        onlyName="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        *)    # unknown option
+        echo "Unknown option: $1"
+        exit 1
+        ;;
+    esac
+done
+
+[[ -n "$onlyName" ]] && disableSubLine=0
 
 A=''
 B=''
@@ -34,6 +67,7 @@ do
   if [[ $line =~ ":" || $count -eq "2" ]] ; then
     A=${array[1]}
 
+    [[ -n "$onlyName" && "$A" != "$onlyName" ]] && continue  # 只解析指定名称的依赖
     [[ disableSubLine -eq 1 && $A =~ "/" ]] && continue # 不显示子组件依赖
     echo '"'$A'"[shape=box]' >> $output
   else
@@ -45,6 +79,7 @@ do
 
     if [[ $A != '' ]]; then
       [[ $B =~ "/" || $A =~ "/" ]] && lineColor="black" && style="dotted"
+      [[ -n "$onlyName" && "$A" != "$onlyName" ]] && continue  # 只解析指定名称的依赖
       echo '"'$A'"'  '->'  "\"$B\"[color=$lineColor, shape=box, style=$style]" >> $output
     else 
       echo '"'$B'"[shape=box]' >> $output
